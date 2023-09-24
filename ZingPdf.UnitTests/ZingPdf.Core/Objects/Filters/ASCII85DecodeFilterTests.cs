7967f0b570871dd6fff80df587206bfdac1fef12
Tom Bowers <tombowers@users.noreply.github.com>
2023-09-24 22:06:23 +1000

    Added some tests for filters.

--- ZingPdf.UnitTests/ZingPdf.Core/Objects/Filters/ASCII85DecodeFilterTests.cs
+++ ZingPdf.UnitTests/ZingPdf.Core/Objects/Filters/ASCII85DecodeFilterTests.cs
@@ -12,7 +12,42 @@ namespace ZingPdf.Core.Objects.Filters
             var action = () => new ASCII85DecodeFilter()
                 .Decode("F(f,-F(K0(F!,O8@<6*nCi\"/8Df-\\>BOr<-ARQ^&BQ%p&");
 
-            action.Should().Throw<ArgumentException>();
+            action.Should().Throw<FilterInputFormatException>();
+        }
+
+        [Fact]
+        public void DecodeThrowsForZInMiddleOf5CharacterSequence()
+        {
+            var action = () => new ASCII85DecodeFilter().Decode("qjzqo~>");
+
+            action.Should().Throw<FilterInputFormatException>();
+        }
+
+        [Fact]
+        public void DecodeIgnoresSpacesInInput()
+        {
+            new ASCII85DecodeFilter()
+                .Decode("<+oiaAKYE%ASrl;+EV:.+CoM2Bk29-H#IgQEb-A    0Df9E*DJ()(DfRH~>")
+                .Should().BeEquivalentTo(Encoding.ASCII.GetBytes("These aren't the droids you're looking for."));
+        }
+
+        [Fact]
+        public void DecodeReplacesZWithEmptyBytes()
+        {
+            new ASCII85DecodeFilter()
+                .Decode("9jqo^zBlbD-~>")
+                .Should().BeEquivalentTo(Encoding.ASCII.GetBytes("Man \0\0\0\0is d"));
+        }
+
+        [Theory]
+        [InlineData("F(f,-F(K0(F!,O8@<6*nCi\"/8Df-\\>BOr<-ARQ^&BQ%p&~>", "she sells seashells on the sea shore")]
+        [InlineData("<+ohcEHPu*CER),Dg-(AAoDo:C3=B4F!,CEATAo8BOr<&@=!2AA8c*5~>", "The quick brown fox jumps over the lazy dog.")]
+        [InlineData("<+oiaAKYE%ASrl;+EV:.+CoM2Bk29-H#IgQEb-A0Df9E*DJ()(DfRH~>", "These aren't the droids you're looking for.")]
+        public void DecodeProducesProperOutput(string encoded, string decoded)
+        {
+            new ASCII85DecodeFilter()
+                .Decode(encoded)
+                .Should().BeEquivalentTo(Encoding.ASCII.GetBytes(decoded));
         }
 
         [Theory]
