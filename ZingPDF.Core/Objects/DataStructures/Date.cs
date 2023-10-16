f93a7dbb9d74bad752cd18f8d288faa511a09f9a
Tom Bowers <tombowers@users.noreply.github.com>
2023-10-16 17:36:55 +1100

    Add real number and date parsing

---
+++ ZingPDF.Core/Objects/DataStructures/Date.cs
@@ -0,0 +1,19 @@
+﻿using ZingPdf.Core.Extensions;
+
+namespace ZingPdf.Core.Objects.DataStructures
+{
+    internal class Date : PdfObject
+    {
+        public Date(DateTime dateTime)
+        {
+            DateTime = dateTime;
+        }
+
+        public DateTime DateTime { get; }
+
+        protected override async Task WriteOutputAsync(Stream stream)
+        {
+            await stream.WriteTextAsync(DateTime.ToString("D:yyyyMMddHHmmsszz'00'"));
+        }
+    }
+}
--- ZingPDF.Core/Parsing/Parser.cs
+++ ZingPDF.Core/Parsing/Parser.cs
@@ -1,7 +1,9 @@
 ﻿using ZingPdf.Core.Objects;
+using ZingPdf.Core.Objects.DataStructures;
 using ZingPdf.Core.Objects.ObjectGroups;
 using ZingPdf.Core.Objects.ObjectGroups.CrossReferenceTable;
 using ZingPdf.Core.Objects.Primitives;
+using ZingPdf.Core.Parsing.DataStructureParsers;
 using ZingPdf.Core.Parsing.ObjectGroupParsers.CrossReferenceTableParsing;
 using ZingPdf.Core.Parsing.ObjectParsers;
 using ZingPdf.Core.Parsing.PrimitiveParsers;
@@ -16,12 +18,14 @@ namespace ZingPdf.Core.Parsing
         private static readonly DictionaryParser _dictionaryParser = new();
         private static readonly ArrayParser _arrayParser = new();
         private static readonly IntegerParser _integerParser = new();
+        private static readonly RealNumberParser _realNumberParser = new();
         private static readonly IndirectObjectReferenceParser _indirectObjectReferenceParser = new();
         private static readonly HexadecimalStringParser _hexadecimalStringParser = new();
         private static readonly CrossReferenceTableParser _xrefTableParser = new();
         private static readonly CrossReferenceSectionParser _xrefSectionParser = new();
         private static readonly CrossReferenceSectionIndexParser _xrefSectionIndexParser = new();
         private static readonly CrossReferenceEntryParser _xrefEntryParser = new();
+        private static readonly DateParser _dateParser = new();
 
         public static IPdfObjectParser<PdfObject> For(Type pdfObjectType)
             => GetParserForType(pdfObjectType);
@@ -39,12 +43,14 @@ namespace ZingPdf.Core.Parsing
                 Type t when t == typeof(Dictionary) => _dictionaryParser,
                 Type t when t == typeof(Objects.Primitives.Array) => _arrayParser,
                 Type t when t == typeof(Integer) => _integerParser,
+                Type t when t == typeof(RealNumber) => _realNumberParser,
                 Type t when t == typeof(IndirectObjectReference) => _indirectObjectReferenceParser,
                 Type t when t == typeof(HexadecimalString) => _hexadecimalStringParser,
                 Type t when t == typeof(CrossReferenceTable) => _xrefTableParser,
                 Type t when t == typeof(CrossReferenceSection) => _xrefSectionParser,
                 Type t when t == typeof(CrossReferenceSectionIndex) => _xrefSectionIndexParser,
                 Type t when t == typeof(CrossReferenceEntry) => _xrefEntryParser,
+                Type t when t == typeof(Date) => _dateParser,
                 _ => throw new ParserException()
             };
         }
