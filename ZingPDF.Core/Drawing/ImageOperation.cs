1f6911a58fed7f30e576fa7d94e0c665a9a6c8d4
Tom Bowers <tombowers@users.noreply.github.com>
2023-11-13 16:43:49 +1100

    Add drawing interfaces

---
+++ ZingPDF.Core/Drawing/ImageOperation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ZingPdf.Core.Drawing
+{
+    public class ImageOperation
+    {
+        [Obsolete("Reserved for deserialisation")]
+        public ImageOperation() { }
+
+        public ImageOperation(byte[] image, Coordinate position)
+            : this(1, image, position) { }
+
+        public ImageOperation(int pageNumber, byte[] image, Coordinate position)
+        {
+            if (pageNumber < 1) throw new ArgumentOutOfRangeException(nameof(pageNumber), "Value must be greater than zero");
+
+            PageNumber = pageNumber;
+            Image = image ?? throw new ArgumentNullException(nameof(image));
+            Position = position ?? throw new ArgumentNullException(nameof(position));
+        }
+
+        /// <summary>
+        /// The page on which to render this operation.
+        /// </summary>
+        [Range(1, int.MaxValue)]
+        public int PageNumber { get; set; } = 1;
+
+        /// <summary>
+        /// The image data to be rendered.
+        /// </summary>
+        [Required]
+        public byte[] Image { get; set; }
+
+        /// <summary>
+        /// The positions at which to render the image;
+        /// </summary>
+        [Required]
+        public Coordinate Position { get; set; }
+    }
+}
---
+++ ZingPDF.Core/Drawing/PathType.cs
@@ -0,0 +1,17 @@
+﻿namespace ZingPdf.Core.Drawing
+{
+    public enum PathType
+    {
+        /// <summary>
+        /// Lines are straight between points.
+        /// </summary>
+        Linear,
+
+        /// <summary>
+        /// Represents a cubic bézier curve.
+        /// <para />
+        /// Each curve along a path is defined by four points: two endpoints, and two control points.
+        /// </summary>
+        Bezier,
+    }
+}
