83e4350630ddbdd3ae37fe1ea29bdeb1acb866e2
Tom Bowers <tombowers@users.noreply.github.com>
2023-11-21 18:27:53 +1100

    General cleanup

--- ZingPDF.Core/Pdf.cs
+++ ZingPDF.Core/Pdf.cs
@@ -10,24 +10,6 @@ using ZingPdf.Core.Objects.Primitives.IndirectObjects;
 
 namespace ZingPdf.Core
 {
-    // When doing incremental update, we need
-    //  - last trailer
-    //  - last xref table
-    //  - access to file stream
-    //  - - e.g. to add page
-    //  - - seek to root page tree node, parse it
-    //  - - add new version of root page tree node to end of file (with extra entry in kids array, incremented generation number)
-    //  - - add new page indirect object
-    //  - - add new xref section referencing the new objects
-    //  - - add new trailer referencing the new xref section and previous
-
-    // When creating new file, we need
-    //  - header
-    //  - list of indirect objects
-
-    // When compressing, encrypting, decrypting, reading file, we need
-    //  - access to all objects
-
     public class Pdf
     {
         private readonly IncrementalUpdateManager _incrementalUpdateManager = new();
@@ -147,6 +129,8 @@ namespace ZingPdf.Core
 
             if (pageNumber > pages.Count()) throw new ArgumentOutOfRangeException(nameof(pageNumber));
 
+            // TODO: make sure the page has the required 'MediaBox' property, whether inherited or explicitly specified.
+
             return pages.ElementAt(pageNumber - 1);
         }
 
@@ -244,133 +228,4 @@ namespace ZingPdf.Core
             throw new NotImplementedException();
         }
     }
-
-    //public class Pdfr
-    //{
-    //    private readonly PdfTraversal _pdfTraversal = new();
-    //    private readonly IndirectObjectManager _indirectObjectManager = new();
-
-    //    private readonly Header _header;
-    //    private readonly IEnumerable<PdfIncrement> _increments;
-
-
-
-    //    /// <summary>
-    //    /// Used internally to create a PDF from a parsed document.
-    //    /// </summary>
-    //    internal Pdfr(Header header, IEnumerable<PdfIncrement> increments)
-    //    {
-    //        _header = header ?? throw new ArgumentNullException(nameof(header));
-    //        _increments = increments ?? throw new ArgumentNullException(nameof(increments));
-
-    //        foreach(var item in increments.SelectMany(i => i.Body))
-    //        {
-    //            _indirectObjectManager.Add(item.Id, item);
-    //        }
-    //    }
-
-    //    public int GetPageCount()
-    //    {
-    //        var trailerDictionary = _pdfTraversal.GetLatestTrailerDictionary(_increments);
-
-    //        var rootPageTreeNode = _pdfTraversal
-    //            .GetRootPageTreeNode(trailerDictionary, _indirectObjectManager);
-
-    //        return rootPageTreeNode.PageCount;
-    //    }
-
-    //    /// <summary>
-    //    /// Get the page at the specified number.
-    //    /// </summary>
-    //    /// <returns>A <see cref="Page"/> instance.</returns>
-    //    public Page GetPage(int pageNumber)
-    //    {
-    //        if (pageNumber < 1) throw new ArgumentOutOfRangeException(nameof(pageNumber));
-
-    //        var trailerDictionary = _pdfTraversal.GetLatestTrailerDictionary(_increments);
-
-    //        var pages = _pdfTraversal.GetPages(trailerDictionary, _indirectObjectManager);
-
-    //        if (pageNumber > pages.Count()) throw new ArgumentOutOfRangeException(nameof(pageNumber));
-
-    //        return pages.ElementAt(pageNumber - 1);
-    //    }
-
-    //    /// <summary>
-    //    /// Add a blank page to the end of the document.
-    //    /// </summary>
-    //    /// <returns>The page number of the new page.</returns>
-    //    public void AppendPage()
-    //    {
-    //        var trailerDictionary = _pdfTraversal.GetLatestTrailerDictionary(_increments);
-    //        var documentCatalog = _pdfTraversal.GetDocumentCatalog(trailerDictionary, _indirectObjectManager);
-
-    //        var pagesCatalogIndirectObject = _indirectObjectManager[documentCatalog.Pages.Id];
-    //        var page = _indirectObjectManager.Create(Page.CreateNew(pagesCatalogIndirectObject.Id.Reference));
-
-    //        var rootPageTreeNode = _pdfTraversal.GetRootPageTreeNode(trailerDictionary, _indirectObjectManager);
-
-    //        // TODO: For now, to simplify adding pages,
-    //        // new pages are appended to the root page tree node.
-    //        // Determine if there's a better way, like ensuring a balanced tree.
-    //        rootPageTreeNode.Kids.Add(page.Id.Reference);
-
-    //        rootPageTreeNode.PageCount++;
-
-    //        _increments.Last().Add(page);
-    //    }
-
-    //    /// <summary>
-    //    /// Insert a blank page at the specified location.
-    //    /// </summary>
-    //    /// <param name="pageNumber">The location at which to insert the page.</param>
-    //    public void InsertPage(int pageNumber)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    /// <summary>
-    //    /// Delete a page.
-    //    /// </summary>
-    //    /// <param name="pageNumber">The location of the page to delete.</param>
-    //    public void DeletePage(int pageNumber)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    /// <summary>
-    //    /// Output the PDF to a new <see cref="MemoryStream"/>.
-    //    /// </summary>
-    //    /// <returns>A <see cref="MemoryStream"/> instance containing the byte data of the PDF. The stream position will be zero.</returns>
-    //    public async Task<MemoryStream> ToStreamAsync()
-    //    {
-    //        var ms = new MemoryStream();
-    //        await WriteAsync(ms);
-
-    //        ms.Position = 0;
-    //        return ms;
-    //    }
-
-    //    /// <summary>
-    //    /// Write the PDF to the supplied <see cref="Stream"/>.
-    //    /// </summary>
-    //    /// <param name="stream"></param>
-    //    /// <returns></returns>
-    //    public async Task WriteAsync(Stream stream)
-    //    {
-    //        if (!stream.CanWrite)
-    //        {
-    //            throw new ArgumentException("Provided stream must be writable", nameof(stream));
-    //        }
-
-    //        await _header.WriteAsync(stream);
-
-    //        foreach (var increment in _increments)
-    //        {
-    //            await increment.WriteAsync(stream);
-    //        }
-
-    //        await stream.FlushAsync();
-    //    }
-    //}
 }
