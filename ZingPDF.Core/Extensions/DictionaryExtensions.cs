a4beac42c8bcdaac05cd356d1d5eb8b36f939820
Tom Bowers <tombowers@users.noreply.github.com>
2024-03-20 11:25:14 +1100

    Fix incremental update xref dictionary merging

--- ZingPDF.Core/Extensions/DictionaryExtensions.cs
+++ ZingPDF.Core/Extensions/DictionaryExtensions.cs
@@ -2,10 +2,14 @@
 {
     internal static class DictionaryExtensions
     {
+        /// <summary>
+        /// Merges the target dictionary into the source dictionary.<para></para>
+        /// Existing values in the source dictionary will be overwritten.
+        /// </summary>
         public static void MergeInto<TKey, TValue>(this IDictionary<TKey, TValue> source, IDictionary<TKey, TValue> target) where TKey : notnull
         {
-            if (source is null) throw new ArgumentNullException(nameof(source));
-            if (target is null) throw new ArgumentNullException(nameof(target));
+            ArgumentNullException.ThrowIfNull(source);
+            ArgumentNullException.ThrowIfNull(target);
 
             source.ToList().ForEach(x => target[x.Key] = x.Value);
         }
