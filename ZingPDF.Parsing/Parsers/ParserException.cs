4f8ec1c259274aea9dd3281b425586e50ac9b4a1
Tom Bowers <tombowers@users.noreply.github.com>
2024-06-11 23:02:08 +1000

    Continue refactoring

---
+++ ZingPDF.Parsing/Parsers/ParserException.cs
@@ -0,0 +1,13 @@
+﻿namespace ZingPDF.Parsing.Parsers
+{
+    [Serializable]
+    public class ParserException : Exception
+    {
+        public ParserException() { }
+        public ParserException(string message) : base(message) { }
+        public ParserException(string message, Exception inner) : base(message, inner) { }
+        protected ParserException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
