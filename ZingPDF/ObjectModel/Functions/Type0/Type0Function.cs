67056e16db3c42cf1f94bdb800fab792a0d5c488
Tom Bowers <tombowers@users.noreply.github.com>
2024-07-12 22:57:36 +1000

    WIP forms. Implementing graphics so we can add form xobjects for wigdet annotation appearance streams.

---
+++ ZingPDF/ObjectModel/Functions/Type0/Type0Function.cs
@@ -0,0 +1,26 @@
+﻿using ZingPDF.ObjectModel.Filters;
+
+namespace ZingPDF.ObjectModel.Functions.Type0
+{
+    /// <summary>
+    /// ISO 32000-2:2020 - 7.10.2 Type 0 (sampled) functions
+    /// </summary>
+    internal class Type0Function : StreamFunction<Type0FunctionDictionary>
+    {
+        // TODO
+
+        public Type0Function(IEnumerable<IFilter>? filters) : base(filters)
+        {
+        }
+
+        protected override Task<Stream> GetSourceDataAsync(Type0FunctionDictionary dictionary)
+        {
+            throw new NotImplementedException();
+        } 
+
+        protected override Task<Type0FunctionDictionary> GetSpecialisedDictionaryAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
