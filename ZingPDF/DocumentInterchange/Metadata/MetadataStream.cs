9a5832f00d6af873462b327778fa5208bda1c3ff
Tom Bowers <tombowers@users.noreply.github.com>
2024-07-17 22:55:09 +1000

    Develop forms support

---
+++ ZingPDF/DocumentInterchange/Metadata/MetadataStream.cs
@@ -0,0 +1,22 @@
+﻿using ZingPDF.ObjectModel.Filters;
+using ZingPDF.ObjectModel.Objects.Streams;
+
+namespace ZingPDF.DocumentInterchange.Metadata
+{
+    internal class MetadataStream : StreamObject<MetadataStreamDictionary>
+    {
+        public MetadataStream(IEnumerable<IFilter>? filters) : base(filters)
+        {
+        }
+
+        protected override Task<Stream> GetSourceDataAsync(MetadataStreamDictionary dictionary)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task<MetadataStreamDictionary> GetSpecialisedDictionaryAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
