c1a830489d1089cdef8ca2050c4dcbdec8095866
Tom Bowers <tombowers@users.noreply.github.com>
2024-07-19 22:28:32 +1000

    Develop forms support

--- ZingPDF/ObjectModel/ContentStreamsAndResources/ContentStream.cs
+++ ZingPDF/ObjectModel/ContentStreamsAndResources/ContentStream.cs
@@ -1,25 +1,37 @@
-﻿using ZingPDF.ObjectModel.Filters;
+﻿using ZingPDF.Extensions;
+using ZingPDF.ObjectModel.Filters;
 using ZingPDF.ObjectModel.Objects.Streams;
 
-namespace ZingPDF.ObjectModel.ContentStreamsAndResources
+namespace ZingPDF.ObjectModel.ContentStreamsAndResources;
+
+/// <summary>
+/// ISO 32000-2:2020 7.8.2 - Content streams
+/// </summary>
+internal class ContentStream<TDictionary> : StreamObject<TDictionary> where TDictionary : class, IStreamDictionary
 {
-    /// <summary>
-    /// ISO 32000-2:2020 7.8.2 - Content streams
-    /// </summary>
-    internal class ContentStream<TDictionary> : StreamObject<TDictionary> where TDictionary : class, IStreamDictionary
+    private readonly IEnumerable<IContentStreamInstruction> _instructions;
+
+    public ContentStream(IEnumerable<IContentStreamInstruction> instructions, IEnumerable<IFilter>? filters) : base(filters)
     {
-        public ContentStream(IEnumerable<IFilter>? filters) : base(filters)
-        {
-        }
-
-        protected override Task<Stream> GetSourceDataAsync(TDictionary dictionary)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override Task<TDictionary> GetSpecialisedDictionaryAsync()
-        {
-            throw new NotImplementedException();
-        }
+        _instructions = instructions ?? throw new ArgumentNullException(nameof(instructions));
     }
+
+    protected override async Task<Stream> GetSourceDataAsync(TDictionary dictionary)
+    {
+        var ms = new MemoryStream();
+
+        foreach (var instruction in _instructions)
+        {
+            await ms.WriteTextAsync(instruction.Operator);
+            await ms.WriteWhitespaceAsync();
+
+            await instruction.Operand.WriteAsync(ms);
+        }
+
+        ms.Position = 0;
+
+        return ms;
+    }
+
+    protected override Task<TDictionary> GetSpecialisedDictionaryAsync() => Task.FromResult((TDictionary)StreamDictionary.Empty());
 }
