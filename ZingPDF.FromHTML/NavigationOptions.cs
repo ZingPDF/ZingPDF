ae672717d8ec43186dd4f294be6d67f25e337859
Tom Bowers <tombowers@users.noreply.github.com>
2024-07-29 13:04:10 +1000

    Finalised initial version of HTML conversion.

--- ZingPDF.FromHTML/NavigationOptions.cs
+++ ZingPDF.FromHTML/NavigationOptions.cs
@@ -4,22 +4,20 @@ namespace ZingPDF.FromHTML
 {
     public class NavigationOptions
     {
-        private NavigationOptions() { }
-
         /// <summary>
         /// When to consider navigation succeeded, defaults to <see cref="WaitUntilNavigation.Load"/>. 
         /// This is a bit flag field, navigation is considered to be successful after all events have been fired.</param>
         /// </summary>
-        public WaitUntil? WaitUntilFlags { get; private set; }
+        public WaitUntil? WaitUntilFlags { get; set; }
 
         /// <summary>
-        /// 
+        /// Maximum navigation time in milliseconds, defaults to 30 seconds, pass <c>0</c> to disable timeout.
         /// </summary>
-        public int? TimeoutExpiration { get; private set; }
+        public int TimeoutExpiration { get; set; } = 1000 * 30;
 
         public static NavigationOptions Default { get; } = new NavigationOptions();
 
-        public static NavigationOptions Timeout(int seconds) => new() { TimeoutExpiration = seconds };
+        public static NavigationOptions Timeout(int milliseconds) => new() { TimeoutExpiration = milliseconds };
         public static NavigationOptions WaitUntil(WaitUntil waitUntil) => new() { WaitUntilFlags = waitUntil };
     }
 }
