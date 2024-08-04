41d494fe621cfa651afbbf3e9089783db1ba6e71
Tom Bowers <tombowers@users.noreply.github.com>
2024-08-04 23:30:22 +1000

    Develop image support

---
+++ ZingPDF/Graphics/Images/ImageMetadata.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ZingPDF.Graphics.Images
+{
+    internal class ImageMetadata
+    {
+        public ImageMetadata(ImageType type, int bitDepth, ColorSpace colorSpace, CompressionType compressionType)
+        {
+            Type = type;
+            BitDepth = bitDepth;
+            ColorSpace = colorSpace;
+            CompressionType = compressionType;
+        }
+
+        public ImageType Type { get; }
+        public int BitDepth { get; }
+        public ColorSpace ColorSpace { get; }
+        public CompressionType CompressionType { get; }
+    }
+}
