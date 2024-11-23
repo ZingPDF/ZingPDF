84bb62069e9856bcd623cabe2f81a776c4ac8510
Tom Bowers <tombowers@users.noreply.github.com>
2024-11-23 14:31:33 +1100

    Centralize test files and add basic smoke tests

---
+++ ZingPdf.UnitTests/Parsing/SmokeTests.cs
@@ -0,0 +1,22 @@
+using Xunit;
+using ZingPDF.UnitTests.TestFiles;
+
+namespace ZingPDF.Parsing;
+
+public class SmokeTests
+{
+    [Theory]
+    [InlineData("testfiles/pdf/combobox-form.pdf")]
+    [InlineData("testfiles/pdf/complex-form.pdf")]
+    [InlineData("testfiles/pdf/form.pdf")]    
+    [InlineData("testfiles/pdf/Ghostscript.pdf")]
+    [InlineData("testfiles/pdf/MikeyFlemingFreelance_Folio.pdf")]
+    [InlineData("testfiles/pdf/minimal.pdf")]
+    [InlineData("testfiles/pdf/test.pdf")]
+    public async Task Parse(string filePath)
+    {
+        var pdf = await PdfParser.OpenAsync(new MemoryStream(Files.ConcurrentRead(filePath)));
+
+        await pdf.GetPageCountAsync();
+    }
+}
