1543d2b46d1eb15ad6e0f10aa2edb613e751070d
Tom Bowers <tombowers@users.noreply.github.com>
2024-11-29 23:33:16 +1100

    WIP fixing pdf merging

--- ZingPDF/Syntax/Objects/Dictionary.cs
+++ ZingPDF/Syntax/Objects/Dictionary.cs
@@ -8,7 +8,7 @@ namespace ZingPDF.Syntax.Objects
     /// <summary>
     /// ISO 32000-2:2020 7.3.7 - Dictionary objects
     /// </summary>
-    public class Dictionary : PdfObject, IReadOnlyDictionary<Name, IPdfObject>
+    public class Dictionary : PdfObject, IDictionary<Name, IPdfObject>
     {
         private readonly Dictionary<Name, IPdfObject> _dictionary;
 
@@ -16,7 +16,7 @@ namespace ZingPDF.Syntax.Objects
         {
             _dictionary = [];
 
-            if (type != null)
+            if (type is not null)
             {
                 _dictionary[Constants.DictionaryKeys.Type] = type;
             }
@@ -28,19 +28,6 @@ namespace ZingPDF.Syntax.Objects
         }
 
         public Name? Type => Get<Name>(Constants.DictionaryKeys.Type);
-
-        #region IReadOnlyDictionary
-
-        public IPdfObject this[Name key] => _dictionary[key];
-        public IEnumerable<Name> Keys => _dictionary.Keys;
-        public IEnumerable<IPdfObject> Values => _dictionary.Values;
-        public int Count => _dictionary.Count;
-        public bool ContainsKey(Name key) => _dictionary.ContainsKey(key);
-        IEnumerator IEnumerable.GetEnumerator() => _dictionary.GetEnumerator();
-        public IEnumerator<KeyValuePair<Name, IPdfObject>> GetEnumerator() => _dictionary.GetEnumerator();
-        public bool TryGetValue(Name key, [MaybeNullWhen(false)] out IPdfObject value) => _dictionary.TryGetValue(key, out value);
-
-        #endregion
 
         /// <summary>
         /// Strongly typed access to the underlying <see cref="PdfObject"/>.
@@ -106,8 +93,28 @@ namespace ZingPDF.Syntax.Objects
             _dictionary[key] = value;
         }
 
+        #region IDictionary
+        public void Add(Name key, IPdfObject value) => ((IDictionary<Name, IPdfObject>)_dictionary).Add(key, value);
+        public bool ContainsKey(Name key) => ((IDictionary<Name, IPdfObject>)_dictionary).ContainsKey(key);
+        public bool Remove(Name key) => ((IDictionary<Name, IPdfObject>)_dictionary).Remove(key);
+        public bool TryGetValue(Name key, [MaybeNullWhen(false)] out IPdfObject value) => ((IDictionary<Name, IPdfObject>)_dictionary).TryGetValue(key, out value);
+        public void Add(KeyValuePair<Name, IPdfObject> item) => ((ICollection<KeyValuePair<Name, IPdfObject>>)_dictionary).Add(item);
+        public void Clear() => ((ICollection<KeyValuePair<Name, IPdfObject>>)_dictionary).Clear();
+        public bool Contains(KeyValuePair<Name, IPdfObject> item) => ((ICollection<KeyValuePair<Name, IPdfObject>>)_dictionary).Contains(item);
+        public void CopyTo(KeyValuePair<Name, IPdfObject>[] array, int arrayIndex) => ((ICollection<KeyValuePair<Name, IPdfObject>>)_dictionary).CopyTo(array, arrayIndex);
+        public bool Remove(KeyValuePair<Name, IPdfObject> item) => ((ICollection<KeyValuePair<Name, IPdfObject>>)_dictionary).Remove(item);
+        public IEnumerator<KeyValuePair<Name, IPdfObject>> GetEnumerator() => ((IEnumerable<KeyValuePair<Name, IPdfObject>>)_dictionary).GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)_dictionary).GetEnumerator();
+        public ICollection<Name> Keys => ((IDictionary<Name, IPdfObject>)_dictionary).Keys;
+        public ICollection<IPdfObject> Values => ((IDictionary<Name, IPdfObject>)_dictionary).Values;
+        public int Count => ((ICollection<KeyValuePair<Name, IPdfObject>>)_dictionary).Count;
+        public bool IsReadOnly => ((ICollection<KeyValuePair<Name, IPdfObject>>)_dictionary).IsReadOnly;
+        public IPdfObject this[Name key] { get => ((IDictionary<Name, IPdfObject>)_dictionary)[key]; set => ((IDictionary<Name, IPdfObject>)_dictionary)[key] = value; }
+        #endregion IDictionary
+
         public static Dictionary Empty => new((Name?)null);
 
         public static implicit operator Dictionary(Dictionary<Name, IPdfObject> value) => new(value);
+        public static implicit operator Dictionary<Name, IPdfObject>(Dictionary? value) => new(value ?? Empty);
     }
 }
