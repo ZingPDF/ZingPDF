46bcd48c3fc0951040e6867186a75e3cff61811b
Tom Bowers <tombowers@users.noreply.github.com>
2025-03-28 18:16:24 +1100

    Add DCT compression for JPEG images

--- ZingPDF/Syntax/Filters/DCTDecodeFilter.cs
+++ ZingPDF/Syntax/Filters/DCTDecodeFilter.cs
@@ -1,27 +1,38 @@
-﻿using ZingPDF.Syntax.Objects;
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Bmp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.PixelFormats;
+using ZingPDF.Syntax.Objects;
 using ZingPDF.Syntax.Objects.Dictionaries;
 
 namespace ZingPDF.Syntax.Filters
 {
-    internal class DCTDecodeFilter : IFilter
+    internal class DCTDecodeFilter(Dictionary? filterParams) : IFilter
     {
-        public DCTDecodeFilter(Dictionary? filterParams)
-        {
-            Params = filterParams;
-        }
-
         public Name Name => Constants.Filters.DCT;
 
-        public Dictionary? Params { get; }
+        public Dictionary? Params { get; } = filterParams;
 
         public byte[] Decode(byte[] data)
         {
-            throw new NotImplementedException();
+            using var inputStream = new MemoryStream(data);
+            using var image = Image.Load<Rgba32>(inputStream);
+            using var outputStream = new MemoryStream();
+
+            image.Save(outputStream, new BmpEncoder());
+
+            return outputStream.ToArray();
         }
 
         public byte[] Encode(byte[] data)
         {
-            throw new NotImplementedException();
+            using var inputStream = new MemoryStream(data);
+            using var image = Image.Load<Rgba32>(inputStream);
+            using var outputStream = new MemoryStream();
+
+            image.Save(outputStream, new JpegEncoder());
+
+            return outputStream.ToArray();
         }
     }
 }
