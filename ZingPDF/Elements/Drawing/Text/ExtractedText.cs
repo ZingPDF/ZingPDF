0d3b2fc82f681c89aab5a077bb4845af70377178
Tom Bowers <tombowers@users.noreply.github.com>
2025-04-05 22:33:07 +1100

    WIP text extraction.

---
+++ ZingPDF/Elements/Drawing/Text/ExtractedText.cs
@@ -0,0 +1,15 @@
+﻿namespace ZingPDF.Elements.Drawing.Text;
+
+public class ExtractedText
+{
+    public required int PageNumber { get; init; }
+    public required string Text { get; init; }
+
+    //"x": 100.5,
+    //"y": 200.2,
+    //"width": 34.5,
+    //"height": 12,
+    //"font": "Helvetica-Bold",
+    //"fontSize": 12,
+    //"rotation": 0
+}
