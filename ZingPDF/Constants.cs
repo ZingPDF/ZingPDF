b57ee75e10a2f0e3e27d744d18435b04b67a5f2f
Tom Bowers <tombowers@users.noreply.github.com>
2025-05-05 18:35:39 +1000

    Refine constants

--- ZingPDF/Constants.cs
+++ ZingPDF/Constants.cs
@@ -73,8 +73,8 @@ internal static class Constants
 
     public static class ButtonStates
     {
-        public static readonly Name On = "Yes";
-        public static readonly Name Off = "Off";
+        public const string On = "Yes";
+        public const string Off = "Off";
     }
 
     public static class Filters
@@ -522,15 +522,15 @@ internal static class Constants
 
     internal static class PdfVersion
     {
-        public static double v1 = 1.0;
-        public static double v1_1 = 1.1;
-        public static double v1_2 = 1.2;
-        public static double v1_3 = 1.3;
-        public static double v1_4 = 1.4;
-        public static double v1_5 = 1.5;
-        public static double v1_6 = 1.6;
-        public static double v1_7 = 1.7;
-        public static double v2 = 2.0;
+        public const double v1 = 1.0;
+        public const double v1_1 = 1.1;
+        public const double v1_2 = 1.2;
+        public const double v1_3 = 1.3;
+        public const double v1_4 = 1.4;
+        public const double v1_5 = 1.5;
+        public const double v1_6 = 1.6;
+        public const double v1_7 = 1.7;
+        public const double v2 = 2.0;
 
         public static double[] All = [v1, v1_2, v1_3, v1_4, v1_5, v1_6, v1_7, v2];
     }
