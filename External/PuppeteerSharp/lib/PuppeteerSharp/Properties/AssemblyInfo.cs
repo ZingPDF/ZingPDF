277ae1a78b60ffbf3866de8e54898ad6fba8bae1
Tom Bowers <tombowers@users.noreply.github.com>
2025-07-30 10:44:38 +1000

    Convert PuppeteerSharp to a NuGet reference

--- External/PuppeteerSharp/lib/PuppeteerSharp/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("ZingPDF.FromHTML")]
-[assembly: InternalsVisibleTo("PuppeteerSharp.Tests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
