fe00ce81fd2aa8e3c81b30c9328d180dcc2b1b2f
Tom Bowers <tombowers@users.noreply.github.com>
2026-01-16 12:41:54 +1100

    Add tests for encrypted PDF handling

---
+++ ZingPDF/Syntax/Encryption/IPdfEncryptionProvider.cs
@@ -0,0 +1,11 @@
+using ZingPDF.Syntax.Objects.Streams;
+using ZingPDF.Syntax;
+
+namespace ZingPDF.Syntax.Encryption;
+
+internal interface IPdfEncryptionProvider
+{
+    Task AuthenticateAsync(string password);
+
+    Task<byte[]> DecryptObjectBytesAsync(ObjectContext context, byte[] data, IStreamDictionary? streamDictionary);
+}
