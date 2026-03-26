28ee48f1f272822e3d1563fb42f52cc56df4c36b
Tom Bowers <tombowers@users.noreply.github.com>
2026-03-26 14:43:12 +1100

    Optimize indirect object and catalog dereference paths

--- ZingPDF/Parsing/Parsers/Objects/IndirectObjectParser.cs
+++ ZingPDF/Parsing/Parsers/Objects/IndirectObjectParser.cs
@@ -4,12 +4,15 @@ using ZingPDF.Graphics.FormXObjects;
 using ZingPDF.Graphics.Images;
 using ZingPDF.Logging;
 using ZingPDF.Syntax;
+using ZingPDF.Syntax.CommonDataStructures;
 using ZingPDF.Syntax.Encryption;
 using ZingPDF.Syntax.FileStructure.CrossReferences.CrossReferenceStreams;
 using ZingPDF.Syntax.FileStructure.ObjectStreams;
 using ZingPDF.Syntax.Objects;
+using ZingPDF.Syntax.Objects.Dictionaries;
 using ZingPDF.Syntax.Objects.IndirectObjects;
 using ZingPDF.Syntax.Objects.Streams;
+using ZingPDF.Syntax.Objects.Strings;
 
 namespace ZingPDF.Parsing.Parsers.Objects
 {
@@ -18,6 +21,13 @@ namespace ZingPDF.Parsing.Parsers.Objects
         private readonly IParserResolver _parserResolver;
         private readonly IParser<Number> _numberParser;
         private readonly IParser<Keyword> _keywordParser;
+        private readonly IParser<Dictionary> _dictionaryParser;
+        private readonly IParser<Name> _nameParser;
+        private readonly IParser<ArrayObject> _arrayParser;
+        private readonly IParser<IndirectObjectReference> _indirectObjectReferenceParser;
+        private readonly IParser<PdfString> _pdfStringParser;
+        private readonly IParser<BooleanObject> _booleanObjectParser;
+        private readonly IParser<Date> _dateParser;
         private readonly ITokenTypeIdentifier _tokenTypeIdentifier;
         private readonly IPdfEncryptionProvider _encryptionProvider;
 
@@ -25,12 +35,26 @@ namespace ZingPDF.Parsing.Parsers.Objects
             IParserResolver parserResolver,
             IParser<Number> numberParser,
             IParser<Keyword> keywordParser,
+            IParser<Dictionary> dictionaryParser,
+            IParser<Name> nameParser,
+            IParser<ArrayObject> arrayParser,
+            IParser<IndirectObjectReference> indirectObjectReferenceParser,
+            IParser<PdfString> pdfStringParser,
+            IParser<BooleanObject> booleanObjectParser,
+            IParser<Date> dateParser,
             ITokenTypeIdentifier tokenTypeIdentifier,
             IPdfEncryptionProvider encryptionProvider)
         {
             _parserResolver = parserResolver;
             _numberParser = numberParser;
             _keywordParser = keywordParser;
+            _dictionaryParser = dictionaryParser;
+            _nameParser = nameParser;
+            _arrayParser = arrayParser;
+            _indirectObjectReferenceParser = indirectObjectReferenceParser;
+            _pdfStringParser = pdfStringParser;
+            _booleanObjectParser = booleanObjectParser;
+            _dateParser = dateParser;
             _tokenTypeIdentifier = tokenTypeIdentifier;
             _encryptionProvider = encryptionProvider;
         }
@@ -48,11 +72,11 @@ namespace ZingPDF.Parsing.Parsers.Objects
             // obj keyword
             _ = await _keywordParser.ParseAsync(stream, context);
 
-            var items = new List<IPdfObject>();
             var parentReference = new IndirectObjectReference(new IndirectObjectId(id, genNumber), ObjectContext.WithOrigin(context.Origin));
             var itemContext = context with { NearestParent = parentReference };
+            IPdfObject? item = null;
 
-            do
+            while (stream.Position < stream.Length)
             {
                 var type = await _tokenTypeIdentifier.TryIdentifyAsync(stream);
 
@@ -61,30 +85,31 @@ namespace ZingPDF.Parsing.Parsers.Objects
                     continue;
                 }
 
-                if (type == typeof(StreamObject<>))
+                item = await ParseSingleItemAsync(type, stream, itemContext);
+                break;
+            }
+
+            if (item is null)
+            {
+                throw new ParserException("Unable to parse indirect object value.");
+            }
+
+            if (item is IStreamDictionary streamDict)
+            {
+                var nextType = await _tokenTypeIdentifier.TryIdentifyAsync(stream);
+                if (nextType == typeof(StreamObject<>))
                 {
                     // It's difficult to reliably identify a stream, which is a dictionary followed by the stream contents.
                     // The token identifier will recognise the stream keyword, at which point we've already parsed the dictionary.
-                    var streamDict = (items.Last() as IStreamDictionary)!;
-                    items.RemoveAt(items.Count - 1);
-
-                    items.Add(await ParseTypedStreamObjectAsync(streamDict, stream, itemContext));
-                    break;
+                    item = await ParseTypedStreamObjectAsync(streamDict, stream, itemContext);
                 }
-
-                IPdfObject item = await _parserResolver.GetParserFor(type).ParseAsync(stream, itemContext);
-
-                if (item is Keyword keyword && keyword == Constants.ObjEnd)
-                {
-                    break;
-                }
-
-                items.Add(item);
-
             }
-            while (stream.Position < stream.Length);
+            else
+            {
+                _ = await _keywordParser.ParseAsync(stream, itemContext);
+            }
 
-            return new IndirectObject(new IndirectObjectId(id, genNumber), items.First()) { ByteOffset = initialStreamPosition };
+            return new IndirectObject(new IndirectObjectId(id, genNumber), item) { ByteOffset = initialStreamPosition };
         }
 
         private async Task<IPdfObject> ParseTypedStreamObjectAsync(IStreamDictionary dict, Stream stream, ObjectContext context)
@@ -108,6 +133,56 @@ namespace ZingPDF.Parsing.Parsers.Objects
 
                 _ => throw new InvalidOperationException($"Unsupported dictionary type: {dict.GetType().Name}"),
             };
+        }
+
+        private async Task<IPdfObject> ParseSingleItemAsync(Type type, Stream stream, ObjectContext context)
+        {
+            if (type == typeof(Dictionary))
+            {
+                return await _dictionaryParser.ParseAsync(stream, context);
+            }
+
+            if (type == typeof(Name))
+            {
+                return await _nameParser.ParseAsync(stream, context);
+            }
+
+            if (type == typeof(Number))
+            {
+                return await _numberParser.ParseAsync(stream, context);
+            }
+
+            if (type == typeof(ArrayObject))
+            {
+                return await _arrayParser.ParseAsync(stream, context);
+            }
+
+            if (type == typeof(IndirectObjectReference))
+            {
+                return await _indirectObjectReferenceParser.ParseAsync(stream, context);
+            }
+
+            if (type == typeof(PdfString))
+            {
+                return await _pdfStringParser.ParseAsync(stream, context);
+            }
+
+            if (type == typeof(BooleanObject))
+            {
+                return await _booleanObjectParser.ParseAsync(stream, context);
+            }
+
+            if (type == typeof(Date))
+            {
+                return await _dateParser.ParseAsync(stream, context);
+            }
+
+            if (type == typeof(Keyword))
+            {
+                return await _keywordParser.ParseAsync(stream, context);
+            }
+
+            return await _parserResolver.GetParserFor(type).ParseAsync(stream, context);
         }
     }
 }
