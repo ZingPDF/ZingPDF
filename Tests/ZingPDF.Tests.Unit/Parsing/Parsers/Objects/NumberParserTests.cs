6b5e5915d3d78dde81e158fd44cfed198e663193
Tom Bowers <tombowers@users.noreply.github.com>
2026-03-27 18:46:34 +1100

    Skip PDF comments before parsing numbers and keywords

--- Tests/ZingPDF.Tests.Unit/Parsing/Parsers/Objects/NumberParserTests.cs
+++ Tests/ZingPDF.Tests.Unit/Parsing/Parsers/Objects/NumberParserTests.cs
@@ -13,6 +13,8 @@ public class NumberParserTests
     [InlineData("1", 1d)]
     [InlineData("595.276000", 595.276000)]
     [InlineData("841.890000", 841.890000)]
+    [InlineData("% comment\r\n12", 12d)]
+    [InlineData(" \r\n% comment between tokens\r\n27", 27d)]
     public async Task ParseAsyncBasic(string input, double expected)
     {
         var output = await new NumberParser()
